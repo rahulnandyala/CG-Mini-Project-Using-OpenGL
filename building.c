42a785d982bcbe16cd3503caf37deb35da1c9efd
Rahul Yedida <yrahul@master-H81M-S1.(none)>
2018-03-14 20:18:04 +0530

    Linted code

--- building.c
+++ building.c
@@ -1,70 +1,78 @@
 #include <GL/glut.h>
 
 /*
-    Description: To draw a building with the co-ordinates of the 			 left corner given.
+    Description: To draw a building with the co-ordinates of the 			 
+    left corner given.
     
     Parameters: 
-		"height" - Floating point value for the heignt of the 				 building.
-		"tx, ty, tz" - The tx, ty and tz values for the 					     bottom left corner of the building. 
+	height: Floating point value for the heignt of the 				 
+		building.
+	tx, ty, tz: The tx, ty and tz values for the 					     
+		bottom left corner of the building. 
     
     Returns: void
 */
 
-GLfloat height = 500;
-GLfloat tx = 0, ty = 0, tz = 0;
-
-void display()
+void display(float height, float tx, float ty, float tz)
 {   
-    glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT);
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
     glColor3f(0.1, 0.1, 0.1);
-    glBegin(GL_POLYGON);//top
-    glVertex3f(0.0+tx, height+ty, 0.0+tz);
-    glVertex3f(100.0+tx, height+ty, 0.0+tz);
-    glVertex3f(100.0+tx, height+ty, 100.0+tz);
-    glVertex3f(0.0+tx, height+ty, 100.0+tz);
+    
+    // Top of the building
+    glBegin(GL_POLYGON);
+    glVertex3f(tx, height + ty, tz);
+    glVertex3f(100 + tx, height + ty, tz);
+    glVertex3f(100 + tx, height + ty, 100 + tz);
+    glVertex3f(tx, height + ty, 100 + tz);
     glEnd();
 
     glColor3f(0.1, 0.1, 0.1);
-    glBegin(GL_POLYGON);//bottom
-    glVertex3f(0.0 +tx, 0.0+ty, 0.0+tz);
-    glVertex3f(100.0 +tx, 0.0+ty, 0.0+tz);
-    glVertex3f(100.0 +tx, 0.0+ty, 100.0+tz);
-    glVertex3f(0.0 +tx, 0.0+ty, 100.0+tz);
+    
+    // Bottom of the building
+    glBegin(GL_POLYGON);
+    glVertex3f(tx, ty, tz);
+    glVertex3f(100 + tx, ty, tz);
+    glVertex3f(100 + tx, ty, 100 + tz);
+    glVertex3f(tx, ty, 100 + tz);
     glEnd();
 
     glColor3f(0.75, 0.75, 0.75);
-    glBegin(GL_POLYGON);//front
-    glVertex3f(0.0 +tx, height+ty, 100.0+tz);
-    glVertex3f(100.0 +tx, height+ty, 100.0+tz);
-    glVertex3f(100.0 +tx, 0.0+ty, 100.0+tz);
-    glVertex3f(0.0 +tx, 0.0+ty, 100.0+tz);
+    
+    // Front of the building
+    glBegin(GL_POLYGON);
+    glVertex3f(tx, height + ty, 100 + tz);
+    glVertex3f(100 + tx, height + ty, 100 + tz);
+    glVertex3f(100 + tx, ty, 100 + tz);
+    glVertex3f(tx, ty, 100 + tz);
     glEnd();
 
     glColor3f(0.75, 0.75, 0.75);
-    glBegin(GL_POLYGON);//back
-    glVertex3f(0.0 +tx, height+ty, 0.0+tz);
-    glVertex3f(100.0 +tx, height+ty, 0.0+tz);
-    glVertex3f(100.0 +tx, 0.0+ty, 0.0+tz);
-    glVertex3f(0.0 +tx, 0.0+ty, 0.0+tz);
+    
+    // Back of the building
+    glBegin(GL_POLYGON);
+    glVertex3f(tx, height + ty, tz);
+    glVertex3f(100 + tx, height + ty, tz);
+    glVertex3f(100 + tx, ty, tz);
+    glVertex3f(tx, ty, tz);
     glEnd();
 
     glColor3f(0.45, 0.45, 0.45);
-    glBegin(GL_POLYGON);//left
-    glVertex3f(0.0 +tx, height+ty, 100.0+tz);
-    glVertex3f(0.0 +tx, height+ty, 0.0+tz);
-    glVertex3f(0.0 +tx, 0.0+ty, 0.0+tz);
-    glVertex3f(0.0 +tx, 0.0+ty, 100.0+tz);
+    
+    // Left
+    glBegin(GL_POLYGON);
+    glVertex3f(tx, height + ty, 100 + tz);
+    glVertex3f(tx, height + ty, tz);
+    glVertex3f(tx, ty, tz);
+    glVertex3f(tx, ty, 100 + tz);
     glEnd();
 
     glColor3f(0.45, 0.45, 0.45);
     glBegin(GL_POLYGON);//right
-    glVertex3f(100.0 +tx, height+ty, 100.0+tz);
-    glVertex3f(100.0 +tx, height+ty, 0.0+tz);
-    glVertex3f(100.0 +tx, 0.0+ty, 0.0+tz);
-    glVertex3f(100.0 +tx, 0.0+ty, 100.0+tz);
+    glVertex3f(100 + tx, height + ty, 100 + tz);
+    glVertex3f(100 + tx, height + ty, tz);
+    glVertex3f(100 + tx, ty, tz);
+    glVertex3f(100 + tx, ty, 100 + tz);
     glEnd();
-
-    gluLookAt(1,1,1,0,0,0,0,1,0);
 
     glFlush();
 }
--- pin.c
+++ pin.c
@@ -1,26 +1,31 @@
 #include <GL/glut.h>
 
 /*
-    Description: To draw a pin with a given center
+    Description: Draws a pin with a given center
     
     Parameters: 
-		"radius" - Floating point value for the head of the 					pin.
-		"c_x, c_y, c_z" - The x, y and z values for the 						  center of the pin's head. 
+	radius: Floating point value for the head of the 					
+		pin.
+	c_x, c_y, c_z: The x, y and z values for the 						  
+		center of the pin's head. 
     
     Returns: void
 */
-void draw_pin(GLfloat radius, GLfloat c_x, GLfloat c_y, GLfloat c_z)
+
+void draw_pin(float radius, float c_x, float c_y, float c_z)
 {
-    glColor3f(0.0, 0.0, 0.0);
+    glColor3f(0, 0, 0);
+    
     glBegin(GL_TRIANGLES);
-    glVertex3f(0.0,-3.5*radius,0.0);
-    glVertex3f(-(radius/2), 0.0,0.0);
-    glVertex3f((radius/2), 0.0,0.0);
+    glVertex3f(0, -3.5 * radius, 0);
+    glVertex3f(-radius / 2, 0, 0);
+    glVertex3f(radius / 2, 0, 0);
     glEnd();
-    glColor3f(1,0,0);
+    
+    glColor3f(1, 0, 0);
     GLUquadric *quad;
     quad = gluNewQuadric();
-    gluSphere(quad,radius,100,20);
-    glTranslatef(c_x,c_y,c_z);
+    gluSphere(quad, radius, 100, 20);
+    glTranslatef(c_x, c_y, c_z);
 }
 
